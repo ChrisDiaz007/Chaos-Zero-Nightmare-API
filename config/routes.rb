ecf5bf3c40a63e9e39f900239218e843fe00d2a6
Christopher Diaz <Christopher.a.diaz007@gmail.com>
2026-02-04 02:08:43 -0500

    Created API Skeleton with user login and jwt authencation

---
+++ config/routes.rb
@@ -0,0 +1,11 @@
+Rails.application.routes.draw do
+  devise_for :users, path: '', path_names: {
+    sign_in: 'login',
+    sign_out: 'logout',
+    registration: 'signup'
+  },
+  controllers: {
+    sessions: 'api/v1/sessions',
+    registrations: 'api/v1/registrations'
+  }
+end
